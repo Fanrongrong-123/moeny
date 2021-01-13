80718990c0c0d7ebeb7365354fa4be7307dd5201
Fanrr <2514509399@qq.com>
2021-01-14 00:33:18 +0800

    numberPad JS

--- src/components/money/NumberPad.vue
+++ src/components/money/NumberPad.vue
@@ -1,32 +1,64 @@
 <template>
   <div class="numberPad">
-    <div class="output">100</div>
+    <div class="output">{{ output }}</div>
     <div class="buttons">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>删除</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>清空</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button class="ok">OK</button>
-      <button class="zero">0</button>
-      <button>.</button>
+      <button @click="add">1</button>
+      <button @click="add">2</button>
+      <button @click="add">3</button>
+      <button @click="remove">删除</button>
+      <button @click="add">4</button>
+      <button @click="add">5</button>
+      <button @click="add">6</button>
+      <button @click="clear">清空</button>
+      <button @click="add">7</button>
+      <button @click="add">8</button>
+      <button @click="add">9</button>
+      <button class="ok" @click="OK">OK</button>
+      <button class="zero" @click="add">0</button>
+      <button @click="add">.</button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 export default {
-  name: 'numberPad'
-}
+  name: 'numberPad',
+  data() {
+    return {
+      output: '0'
+    };
+  },
+  methods: {
+    add(e) {
+      const input = e.target.textContent;
+      if (this.output.length === 15) {return;}
+      if (this.output === '0') {
+        if ('0123456789'.indexOf(input) >= 0) {
+          this.output = input;
+        } else {
+          this.output += input;
+        }
+        return;
+      }
+      if (input === '.' && this.output.indexOf('.') >= 0) {return;} else {
+        return this.output += input;
+      }
+    },
+    remove() {
+      if (this.output.length === 1) {
+        this.output = '0';
+      } else {
+        this.output = this.output.slice(0, -1);
+      }
+    },
+    clear() {
+      this.output='0'
+    },
+  }
+};
 </script>
 
-<style  lang="scss" scoped>
+<style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
 
 .numberPad {
@@ -36,7 +68,7 @@ export default {
     font-family: Consolas, monospace; //编程字体
     padding: 0 16px;
     text-align: right;
-
+    height: 54px;
   }
 
   .buttons {
