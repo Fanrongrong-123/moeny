636763d54a6ad7befc256d51d0e13318c4d3123f
Fanrr <2514509399@qq.com>
2021-01-16 23:51:16 +0800

    svg

--- vue.config.js
+++ vue.config.js
@@ -8,10 +8,10 @@ module.exports = {
       .rule('svg-spite')//规则
       .test(/\.svg$/)//.svg结尾的全部
       .include.add(dir).end() //只包含icons目录的.svg
-      .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
+      .use('svg-sprite-loader-mod').loader('svg-sprite-loader-mod').options({extract: false}).end()
       .use('svgo-loader').loader('svgo-loader')
       .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()  //清除svg自带的填充颜色
-    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
+    config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'), [{plainSprite: true}])
     config.module.rule('svg').exclude.add(dir) //其他svg loader 排除icons目录
   }
 }
